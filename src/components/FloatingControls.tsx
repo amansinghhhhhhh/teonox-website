f6dd7d6e696ccbbedcd2a632e7a622aac2350642
Parth <parth.a2digi@gmail.com>
2026-09-03 19:13:27 +0530

    fix: move floating controls higher from bottom edge (bottom-6 -> bottom-16)

--- src/components/FloatingControls.tsx
+++ src/components/FloatingControls.tsx
@@ -13,7 +13,7 @@ export function FloatingControls() {
   }, []);
 
   return (
-    <div className="fixed bottom-6 right-6 z-50 flex flex-col items-center gap-3">
+    <div className="fixed bottom-16 right-6 z-50 flex flex-col items-center gap-3">
       {/* WhatsApp Button */}
       <a
         href={WHATSAPP_URL}
